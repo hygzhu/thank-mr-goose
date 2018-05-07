1b1695cac44c7671e9ac7a43ab95441898a51e6f
Ron Li <runlin.li@gmail.com>
2018-05-07 19:34:14 -0400

    Add base UI interface
    Implemented:
    - Friends page
    - Collection Page
    - Map page
    - Login page

--- client/src/containers/app/index.js
+++ client/src/containers/app/index.js
@@ -2,17 +2,25 @@ import React from 'react';
 import { Route, Link } from 'react-router-dom'
 import Home from '../home'
 import Login from '../login'
+import Map from '../map'
+import Friends from '../friends'
 
 const App = () => (
   <div>
     <header>
-      <Link to="/">Home</Link>
-      <Link to="/login">Login</Link>
+      <h1>Thank Mr Goose</h1>
+      <div>
+        <div><Link to="/">My Collection</Link></div>
+        <div><Link to="/friends">Friends</Link></div>
+        <div><Link to="/login">Temp Login Link</Link></div>
+      </div>
     </header>
 
     <main>
       <Route exact path="/" component={Home} />
       <Route exact path="/login" component={Login} />
+      <Route exact path="/map" component={Map} />
+      <Route exact path="/friends" component={Friends} />
     </main>
   </div>
 )
--- client/src/containers/friends/index.js
+++ client/src/containers/friends/index.js
@@ -2,34 +2,24 @@ import React from 'react'
 import { push } from 'react-router-redux'
 import { bindActionCreators } from 'redux'
 import { connect } from 'react-redux'
-import {
-  increment,
-  incrementAsync,
-  decrement,
-  decrementAsync
-} from '../../modules/counter'
 
-const Home = props => (
+const Friends = (props) => (
   <div>
+    <div>
+      <img src={"ron.png"}/><h5>Ron Li : Offline</h5><button>View Profile</button>
+    </div>
     <p><button onClick={() => props.viewMap()}>View Map</button></p>
   </div>
 )
 
 const mapStateToProps = state => ({
-  count: state.counter.count,
-  isIncrementing: state.counter.isIncrementing,
-  isDecrementing: state.counter.isDecrementing
 })
 
 const mapDispatchToProps = dispatch => bindActionCreators({
-  increment,
-  incrementAsync,
-  decrement,
-  decrementAsync,
   viewMap: () => push('/map')
 }, dispatch)
 
 export default connect(
   mapStateToProps,
   mapDispatchToProps
-)(Home)
+)(Friends)
