aba18e06507fc1c9ded8a8292b1177220674faee
Ron Li <runlin.li@gmail.com>
2018-05-10 22:24:27 -0400

    Modify styling

--- client/src/containers/friends/index.js
+++ client/src/containers/friends/index.js
@@ -6,14 +6,18 @@ import { connect } from 'react-redux'
 import './friends.css'
 
 const Friends = (props) => (
-  <div className={"friendsWrapper"}>
-    <h3 className={"friendsHeader"}>Friends</h3>
-    <div>
-      <img className={"inlineBlock"} src={"ron.png"} alt="profile"/>
-      <div className={"inlineBlock playerName"}>Ron Li : Offline</div>
-      <button className={"inlineBlock"}>View Profile</button>
+  <div>
+    <div className={"friendsWrapper"}>
+      <h3 className={"friendsHeader"}>Friends</h3>
+      <div>
+        <img className={"inlineBlock"} src={"ron.png"} alt="profile"/>
+        <div className={"inlineBlock playerName"}>Ron Li : Offline</div>
+        <button className={"inlineBlock"}>View Profile</button>
+      </div>
     </div>
-    <button onClick={() => props.viewMap()}>View Map</button>
+    <div className={"viewMap"}>
+      <button onClick={() => props.viewMap()}>View Map</button>
+    </div>
   </div>
 )
 
